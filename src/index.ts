356339ceed7b858a609602ffc4fa0f6735df112e
Bluebeel <saikouah@gmail.com>
2022-09-30 18:25:34 +0200

    fix: add missing export class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { AnalyticsEngineDB } from './AnalyticsEngine'
+import { AnalyticsQueryBuilder } from './AnalyticsBuilder'
 import { DatabaseQueryBuilder } from './DatabaseBuilder'
 import { D1QB } from './Databases'
 import { OrderTypes, FetchTypes } from './enums'
 import { Where, Insert, Update, Delete, SelectAll, SelectOne }  from './interfaces'
 
-export { DatabaseQueryBuilder, D1QB, OrderTypes, FetchTypes, Where, Insert, Update, Delete, SelectAll, SelectOne }
+export { AnalyticsEngineDB, AnalyticsQueryBuilder, DatabaseQueryBuilder, D1QB, OrderTypes, FetchTypes, Where, Insert, Update, Delete, SelectAll, SelectOne }
